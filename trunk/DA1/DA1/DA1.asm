49bc099c153b9dd2efb171c4bf57865bd821a4d4
sanch512@unlv.nevada.edu <sanch512@unlv.nevada.edu@abc429ab-2583-81f5-7df8-f7d3c492769a>
2015-02-22 09:05:37 +0000

    Design Assignment 1 Finished

--- trunk/DA1/DA1/DA1.asm
+++ trunk/DA1/DA1/DA1.asm
@@ -21,20 +21,81 @@ simultaneously.
 */ 
 .INCLUDE "m328pdef.inc"
 
-
 LDI R25, 25		;load 25 as counter
+LDI R24, 0		;zero register
 
-LDI R28, LOW(RAMEND)	;low byte of ramend
-LDI R29, HIGH(RAMEND)	;high byte of ramend
+LDI YL, LOW(RAMEND)	;low byte of ramend
+LDI YH, HIGH(RAMEND) ;high byte of ramend
 
-LDI R30, 0x60	;low byte of rambegin
-LDI R31, 0x00	;high byte of rambegin
+LDI ZL, 0x60	;low byte of rambegin
+LDI ZH, 0x00	;high byte of rambegin
 
-ADC R28, R30	;low
-ADC R29, R31	;high
+ADC YL, ZL	;add low byte (ramend + rambegin)
+ADC YH, ZH	;add high byte
 
-LSR R28
-ASR R29
+LSR YH		;divide by 2
+ROR YL		;Y = ram middle = (rambegin + ramend)/2
+
+;clear registers to store sum of numbers divisible by 7
+LDI XL, 0	;low
+LDI XH, 0	;high
+
+;clear registers to store sum of numbers divisible by 3
+LDI ZL, 0	;low
+LDI ZH, 0	;high
+
+loop: 
+MOV R16, YL ;copy contents of YL into R16
+ST Y, R16   ;copy contents of R16 into location pointed by Y
+
+MOV R17, R16 ;copy R16 to check if div by 7
+divBy7:
+CPI R17, 0 ;check if remainder is 0 (means number is divisible by 7)
+BREQ doneRemainder7 ;skip next subtraction if remainder is 0
+SUBI R17, 7	 ;subtract 7
+BRCC divBy7 ;branch if > 0 
+doneRemainder7:
+
+CPI R17, 0 ;check if remainder is 0 (div by 7)
+BRNE notBy7 ;skip addition if number not divisible by 7
+ADC XL, R16 ;add number to sum of numbers divisible by 7
+ADC XH, r24	;add carry if any
+notBy7:
+
+MOV R17, R16 ;copy R16 to check if div by 3
+divBy3:
+CPI R17, 0   ;check if remainder is 0 (means number is divisible by 3)
+BREQ doneRemainder3 ;skip next subtraction if remainder is 0
+SUBI R17, 3  ;subtract 3
+BRCC divBy3  ;branch if > 0
+doneRemainder3:
+
+CPI R17, 0	;check if remainder is 0 (div by 3)
+BRNE notBy3 ;skip addition if number not divisible by 3
+ADC ZL, R16	;add number to sum of numbers divisible by 3
+ADC ZH, R24	;add carry if any
+notBy3:
+
+ADIW YH:YL, 1	;point to next location in ram
+DEC R25			;decrement counter
+CPI R25, 0		;stops looping after storing 25 numbers
+BRNE loop		;loop back for next number
+
+ADIW XH:XL, 0	;pause here to check for overflow
+BRCS overflow7	;check carry for overflow
+MOV R20, XL		;if no overflow, save sum of numbers div by 7 into R21:R20
+MOV R21, XH
+overflow7:
+
+
+ADIW ZH:ZL, 0	;pause here to check for overflow
+BRCS overflow3	;check carry for overflow
+MOV R23, ZL		;if no overflow, save sum of numbers div by 3 into R24:R23
+MOV R24, ZH
+overflow3:
+
+
+
 
 
 
